096462cca0ff60689123f5cdac426e288b3db269
David Császár <dcsaszar@users.noreply.github.com>
2025-04-22 19:34:11 +0200

    Fix type errors in spec

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -14,9 +14,9 @@ describe("scrollToFragment", () => {
       <a href="index.html#bottom10400" id="same">Same page</a>
       <a href="#bottom10400" id="hashOnly">Hash only</a>
       <a href="#bottom10400"><span id="spanInA">Nested</span></a>
-      `
+      `,
     );
-    history.replaceState(null, null, "index.html");
+    history.replaceState(null, "", "index.html");
     window.scrollTo(0, 333);
     wait(done);
   });
@@ -37,7 +37,7 @@ describe("scrollToFragment", () => {
     describe("clicking a link to a different page", () => {
       beforeEach((done) => {
         window.scrollTo(0, 444);
-        document.getElementById("other").click();
+        document.getElementById("other")?.click();
         wait(done);
       });
 
@@ -49,7 +49,7 @@ describe("scrollToFragment", () => {
     describe("clicking a hash link to the same page", () => {
       beforeEach((done) => {
         window.scrollTo(0, 444);
-        document.getElementById("same").click();
+        document.getElementById("same")?.click();
         wait(done);
       });
 
@@ -67,7 +67,7 @@ describe("scrollToFragment", () => {
       beforeEach((done) => {
         window.scrollTo(0, 444);
         document.addEventListener("click", listener);
-        document.getElementById("same").click();
+        document.getElementById("same")?.click();
         wait(done);
       });
 
@@ -81,7 +81,7 @@ describe("scrollToFragment", () => {
 
   describe("with a URL hash but no matching fragment", () => {
     beforeEach((done) => {
-      history.replaceState(null, null, "index.html#barbaz");
+      history.replaceState(null, "", "index.html#barbaz");
       scrollToFragment();
       wait(done);
     });
@@ -94,7 +94,7 @@ describe("scrollToFragment", () => {
       beforeEach((done) => {
         document
           .getElementById("bottom10400")
-          .insertAdjacentHTML("beforebegin", "<h1 id='barbaz'>H1</h1>");
+          ?.insertAdjacentHTML("beforebegin", "<h1 id='barbaz'>H1</h1>");
         wait(done);
       });
 
@@ -107,7 +107,7 @@ describe("scrollToFragment", () => {
   describe("clicking an element wrapped by a hash link", () => {
     beforeEach((done) => {
       scrollToFragment({ scrollIntoView: () => window.scrollTo(0, 123) });
-      document.getElementById("spanInA").click();
+      document.getElementById("spanInA")?.click();
       wait(done);
     });
 
@@ -130,7 +130,7 @@ describe("scrollToFragment", () => {
   describe("with scrollIntoView", () => {
     beforeEach((done) => {
       scrollToFragment({ scrollIntoView: () => window.scrollTo(0, 123) });
-      document.getElementById("hashOnly").click();
+      document.getElementById("hashOnly")?.click();
       wait(done);
     });
 
@@ -150,7 +150,7 @@ describe("scrollToFragment", () => {
           history: this.history,
           scrollIntoView: () => window.scrollTo(0, 123),
         });
-        document.getElementById("hashOnly").click();
+        document.getElementById("hashOnly")?.click();
         wait(done);
       });
 
